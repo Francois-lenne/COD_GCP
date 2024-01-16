c82a5a61116d7bf0cb75b2aa4c5973a8f66e99b1
François Lenne <francoislenne@Mac-mini-de-Francois.local>
2024-01-16 23:26:07 +0100

    archivage du projet

--- load_warzone_data-bq.py
+++ load_warzone_data-bq.py
@@ -1,6 +1,8 @@
 import asyncio
 import nest_asyncio
 from cod_api import API, platforms
+import pandas as pd
+from pandas import json_normalize
 
 # Initiate the API class
 api = API()
@@ -8,15 +10,20 @@ api = API()
 # Allow nested use of asyncio.run() in Jupyter Notebook
 nest_asyncio.apply()
 
-async def example():
+async def retrieve_cod_data():
     # Log in with the SSO token
     await api.loginAsync('')
 
     # Retrieve combat history
-    hist = await api.Warzone.combatHistoryAsync(platforms.Activision, "RIDbrud#8174385")  # Returns data of type dict
+    hist = await api.ModernWarfare.combatHistoryAsync(platforms.Activision, "yupex#9664411")  # Returns data of type dict
 
-    # Print results to the console
-    print(hist)
+    # Normalize the nested dictionary and convert it to a pandas DataFrame
+    df = json_normalize(hist["data"]["matches"])
+
+    # print(df.head())
+    return df
 
 # Run the asynchronous code using asyncio.run()
-asyncio.run(example())
+df = asyncio.run(retrieve_cod_data())
+
+df.to_csv('warzone_data2.csv', index=False)
